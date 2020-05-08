dc4bc68e2f375199e4eb4db0a4320d430a5bb098
Andrey Burlakov <andrburl2@yandex.ru>
2020-05-08 21:51:48 +0300

    Updates

---
+++ precache-manifest.dfdb15043bfc80001d6136eb5c11b663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f4012d7983b74487830343b7e9c06f",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "c12acb6a4cc2499ba7eb",
+    "url": "/React-Project/static/css/main.cf525931.chunk.css"
+  },
+  {
+    "revision": "b629145d06f30b6751b7",
+    "url": "/React-Project/static/js/2.61339d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Project/static/js/2.61339d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12acb6a4cc2499ba7eb",
+    "url": "/React-Project/static/js/main.5a4d80fa.chunk.js"
+  },
+  {
+    "revision": "d897c65b83982796f034",
+    "url": "/React-Project/static/js/runtime-main.21b5ffc5.js"
+  }
+]);
